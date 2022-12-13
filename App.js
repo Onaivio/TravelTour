3431be3705e43a90fcffb439c408319682a50a58
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-13 13:43:14 +0100

    ..

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import {  Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 import { Discover, HomeScreen, ItemScreen } from "./screens/";
 
@@ -9,10 +9,11 @@ export default function App() {
     <TailwindProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Discover" component={Discover} />
+          <Stack.Screen name="ItemScreen" component={ItemScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-   </TailwindProvider>
+    </TailwindProvider>
   );
 }
-
